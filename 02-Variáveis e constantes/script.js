be575a20ca377dbad4c00aa7e5e4a5069b7a64b1
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-02 23:01:08 -0300

    Aula 04 - Declaração e atribuição de variáveis

---
+++ 02-Variáveis e constantes/script.js
@@ -0,0 +1,13 @@
+//Declara variável sem valor
+var user;
+
+//undefined
+console.log(user);
+
+//Declara com valor
+var email = 'marquesdanilocarlos@gmail.com';
+console.log(email);
+
+//Substitui o valor da variável
+email = 'marquesdanilocarlos@hotmail.com';
+console.log(email);
