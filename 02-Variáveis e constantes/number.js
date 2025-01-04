e0c368ace9cdb695f2d788b2fc5245dd0262e184
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-03 23:47:57 -0300

    Aula 09 - Number

---
+++ 02-Variáveis e constantes/number.js
@@ -0,0 +1,13 @@
+//Number
+console.log(typeof 13452);
+
+//Inteiro positivo
+let numberOne = 10;
+//Inteiro negativo
+let numberTwo = -36;
+//Float
+let numberThree = 50.369;
+
+//Not a Number (NaN) - é do tipo Number tbm
+let nan = numberThree/'Danilo'
+console.log(nan, typeof nan);
