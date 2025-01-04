00a79bd264664a04e8264f7f27e20f8b82f9af72
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-03 23:54:38 -0300

    Aula 11 - Undefined e Null

---
+++ 02-Variáveis e constantes/undefined-null.js
@@ -0,0 +1,9 @@
+//Undefined - Valor da váriavel não foi definido
+//Typeof = 'undefined'
+let emptness;
+console.log(emptness, typeof emptness)
+
+//Null - Conteúdo da variável vazio intencionalmente
+//Typeof = object
+let empty = null;
+console.log(empty, typeof empty)
