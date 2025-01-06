d37d69c267d66f3ad551aa6cc293ae7a11f01d6a
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-05 21:46:13 -0300

    Aula 17 - Maior ou Igual e Menor ou igual

--- 03-expressoes-operadores/logicos.js
+++ 03-expressoes-operadores/logicos.js
@@ -12,3 +12,14 @@ console.log(one != two);
 console.log(one != 1);
 console.log(one != "1");
 console.log(one !== "1");
+
+let balance = 500;
+let payment = 120;
+
+//Maior ou igual
+console.log(balance > payment);
+console.log(balance >= payment);
+
+//Menor ou igual
+console.log(balance < payment);
+console.log(balance <= payment);
