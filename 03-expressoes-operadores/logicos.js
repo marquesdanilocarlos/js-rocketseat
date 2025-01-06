c92acb84fb6930c4cab8fa21e65ecdd5fab1316c
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-05 21:43:32 -0300

    Aula 16 - Igual a e diferente de

---
+++ 03-expressoes-operadores/logicos.js
@@ -0,0 +1,14 @@
+let one = 1;
+let two = 2;
+
+//Igualdade
+console.log(one == two);
+console.log(one == 1);
+console.log(one == "1");
+console.log(one === "1");
+
+//Diferença
+console.log(one != two);
+console.log(one != 1);
+console.log(one != "1");
+console.log(one !== "1");
