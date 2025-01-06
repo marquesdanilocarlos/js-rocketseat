a03417b29d7e630ae8e66bb2f483d914124cf6c5
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-05 21:30:24 -0300

    Aula 14 - Operadores de incremento e decremento

---
+++ 03-expressoes-operadores/incremento-decremento.js
@@ -0,0 +1,22 @@
+let number = 10;
+
+//Incremento
+//Pós incremento
+console.log(number++);
+
+//Pré incremento
+console.log(++number);
+
+//Pós decremento
+console.log(number--);
+
+//Pré decremento
+console.log(--number);
+
+//Incremento com valores maiores que 1
+number += 10;
+console.log(number);
+
+//Decremento com valores maiores que 1
+number -= 15;
+console.log(number);
