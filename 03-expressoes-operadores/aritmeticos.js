9bc0dcf277dba35a7737b09b563fd476bde93388
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-05 21:24:51 -0300

    Aula 13 - Operadores aritmeticos

---
+++ 03-expressoes-operadores/aritmeticos.js
@@ -0,0 +1,24 @@
+//Operadores aritméticos
+
+//Soma diferente de concatenação
+
+let soma = 12+8;
+let concat = 12 + "8"
+console.log(soma, concat);
+
+
+let subtracao = 54-36;
+console.log(subtracao);
+
+
+let multiplicacao = 12*36;
+console.log(multiplicacao);
+
+let divisao = 65/5;
+console.log(divisao);
+
+let mod = 17%2;
+console.log(mod);
+
+let potencia = 3**3;
+console.log(potencia);
