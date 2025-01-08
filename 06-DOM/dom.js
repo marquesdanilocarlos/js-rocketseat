69b53660c25a3676d24c8f417e655d5904e06624
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-07 21:40:57 -0300

    Aula 30 - Selecionar elementos no DOM

---
+++ 06-DOM/dom.js
@@ -0,0 +1,13 @@
+console.log(document.title);
+
+//Acessar elementos pelo id
+let item1 = document.getElementById('li-1');
+console.dir(item1);
+
+//Acessar elementos pela class
+let guests = document.getElementsByClassName('guest');
+console.log(guests);
+
+//Acessar elementos pela tag
+let guestsTag = document.getElementsByTagName('li');
+console.log(guestsTag);
