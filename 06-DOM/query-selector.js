14280ff1d0763ca817f8a326d6aad752868386c1
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-07 21:44:22 -0300

    Aula 31 - Query selector

---
+++ 06-DOM/query-selector.js
@@ -0,0 +1,7 @@
+//Buscar só um elemento pelo id
+const guest = document.querySelector('#guest-2');
+console.log(guest);
+
+//Buscar vários elementos pela classe
+const guestsQuerySelector = document.querySelectorAll('.guest');
+console.log(guestsQuerySelector);
