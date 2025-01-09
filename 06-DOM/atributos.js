2469508e876e4a4b6c5a92da944b4ca6a1a3b3c3
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-08 23:31:12 -0300

    Aula 35 - Manipulação de atributos

---
+++ 06-DOM/atributos.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+
+//Adicionar ou atualizar atributo
+input.setAttribute('disabled', true);
+input.setAttribute('type', 'date');
+
+//Remover atributo
+input.removeAttribute('id');
