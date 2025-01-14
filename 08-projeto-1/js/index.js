84f31424e698c41cc0cf11dde5453e4a1c437464
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-13 23:49:47 -0300

    Aula 45 - Obtenção do valor da moeda selecionada

--- 08-projeto-1/js/index.js
+++ 08-projeto-1/js/index.js
@@ -1,6 +1,14 @@
+const form = document.querySelector('#form');
 const amount = document.querySelector('#amount');
+const currency= document.querySelector('#currency');
 
 amount.addEventListener('input', e => {
     const hasCharactersRegex = /\D+/g;
     amount.value = amount.value.replace(hasCharactersRegex, '');
 });
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    console.log(currency.value);
+};
