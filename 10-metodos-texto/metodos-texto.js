e496ddf0b4246c4074fc577f704a5ef0bf7bc3f7
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-22 23:16:39 -0300

    Aula 62 - padStart e padEnd

--- 10-metodos-texto/metodos-texto.js
+++ 10-metodos-texto/metodos-texto.js
@@ -16,3 +16,12 @@ console.log(message.slice(0, 5));
 console.log(message.slice(-11));
 
 console.log('  Texto com espaço    '.trim());
+
+
+let creditCardNumber = '1234567812344298';
+let lastDigits = creditCardNumber.slice(-4);
+let maskedNumber = lastDigits.padStart(creditCardNumber.length, 'X');
+console.log(maskedNumber);
+
+let number = '123';
+console.log(number.padEnd(10, '#'));
