9658cd97cbc56d674cfd5b5f3a33f91a43d20e0d
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-22 23:10:40 -0300

    Aula 61 - replace, slice, trim

--- 10-metodos-texto/metodos-texto.js
+++ 10-metodos-texto/metodos-texto.js
@@ -9,3 +9,10 @@ console.log(message.length);
 
 let value = 12695;
 console.log(value.toString().length);
+
+console.log(message.replace('Javascript', 'PHP'));
+
+console.log(message.slice(0, 5));
+console.log(message.slice(-11));
+
+console.log('  Texto com espaço    '.trim());
