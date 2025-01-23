b35ca8b4a62169c6c1b552f9408d46c661d95d76
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-22 22:58:16 -0300

    Aula 60 - Tamanhos de string com length

--- 10-metodos-texto/metodos-texto.js
+++ 10-metodos-texto/metodos-texto.js
@@ -4,3 +4,8 @@ console.log(message);
 
 console.log(message.toUpperCase());
 console.log(message.toLowerCase());
+
+console.log(message.length);
+
+let value = 12695;
+console.log(value.toString().length);
