8026d2528be843905479af178e65e85f50922db4
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-23 20:47:53 -0300

    Aula 66 - Quebra de string para array

--- 11-array/arrays.js
+++ 11-array/arrays.js
@@ -5,3 +5,7 @@ console.log(fruits.length);
 console.log(fruits[1]);
 
 console.log(fruits[fruits.length - 1]);
+
+let fullName = "Danilo Carlos Marques";
+console.log(fullName.split(' '))
+console.log(Array.from(fullName))
