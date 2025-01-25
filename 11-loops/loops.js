6d3d23e31fab299d7e8bb3cba4beff1c4c9f332e
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-24 23:00:47 -0300

    Aula 72 - Loop do while

--- 11-loops/loops.js
+++ 11-loops/loops.js
@@ -1,5 +1,13 @@
-let execute  = true;
+/*let execute  = true;
 
 while(execute){
     execute = confirm('Deseja continuar?');
-}
+}*/
+
+
+let value = 1;
+
+do {
+    value++;
+    console.log(value);
+} while (value < 10);
