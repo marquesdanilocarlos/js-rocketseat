ce0da51f703e5203263d033e6b707e83579c0740
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-24 23:19:00 -0300

    Aula 77 - Continue

--- 11-loops/loops.js
+++ 11-loops/loops.js
@@ -46,9 +46,10 @@ for (let value of students) {
 }*/
 
 for (let i = 1; i <= 10; i++) {
-    console.log(i);
 
-    if (i === 5) {
-        break;
+    if (i%2 !== 0) {
+        continue;
     }
+    
+    console.log(i);
 }
