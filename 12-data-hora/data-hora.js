4ae8d4e7c6fd5e29d54f81806b5eae4960ebbbcf
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-26 12:59:53 -0300

    Aula 79 - Criação de data por string

--- 12-data-hora/data-hora.js
+++ 12-data-hora/data-hora.js
@@ -6,3 +6,19 @@ console.log(new Date(0));
 
 //Obtem o número em milissegundos
 console.log(new Date().getTime());
+
+
+let specificDate = new Date(2025, 0, 26);
+console.log(specificDate);
+
+let specificDateTime = new Date(2025, 0, 26, 12, 47, 0);
+console.log(specificDateTime);
+
+let dateByString = new Date("2026-01-26");
+console.log(dateByString);
+
+let dateByUTCString = new Date("2025-01-26T12:48:00");
+console.log(dateByUTCString);
+
+let dateByLongString = new Date("January 26, 2025");
+console.log(dateByLongString);
