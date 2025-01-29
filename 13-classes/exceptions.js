fac2daa583ae0a3f89a67a2b673e626eab81c427
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-28 21:19:21 -0300

    Aula 93 - Classe de erro customizado

--- 13-classes/exceptions.js
+++ 13-classes/exceptions.js
@@ -1,3 +1,10 @@
+class MyCustomError extends Error {
+    constructor(message) {
+        super();
+        this.message = message;
+    }
+}
+
 let obj = [];
 let index = 300;
 try {
@@ -7,14 +14,17 @@ try {
         throw new Error('17 não está disponível.');
     }*/
 
-    if (index > 99) {
+    /*if (index > 99) {
         throw new RangeError('Número está fora do intervalo.');
+    }*/
+
+    if(obj.length === 0){
+        throw new MyCustomError('Meu erro customizado.');
     }
 
 } catch (error) {
     if (error instanceof TypeError) {
         console.log('Método indisponível');
-
     } else {
         console.log(error.message);
     }
