32c1282aca2de80af9929e0c3745775eb58b5d46
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-01 00:18:52 -0300

    Aula 103 - Adição do valor no item de lista

--- 14-projeto-2/scripts.js
+++ 14-projeto-2/scripts.js
@@ -38,25 +38,34 @@ form.onsubmit = e => {
 
 function addExpense(newExpense) {
     try {
+        //Item da lista
         const expenseItem = document.createElement('li');
         expenseItem.classList.add('expense');
 
+        //Ícone do item
         const expenseIcon = document.createElement('img');
         expenseIcon.setAttribute('src', `img/${newExpense.categoryId}.svg`);
         expenseIcon.setAttribute('alt', newExpense.categoryName);
 
+        //Caixa do item
         const expenseInfo = document.createElement('div');
         expenseInfo.classList.add('expense-info');
 
+        //Título e categoria do item
         const expenseName = document.createElement('strong');
         expenseName.textContent = newExpense.expense;
 
         const expenseCategory = document.createElement('span');
         expenseCategory.textContent = newExpense.categoryName;
 
+        //Informações de valor do item
+        const expenseAmount = document.createElement('span');
+        expenseAmount.classList.add('expense-amount');
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace('R$', '')}`;
+
         expenseInfo.append(expenseName, expenseCategory);
 
-        expenseItem.append(expenseIcon, expenseInfo);
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
         expenseList.append(expenseItem);
 
     } catch (error) {
