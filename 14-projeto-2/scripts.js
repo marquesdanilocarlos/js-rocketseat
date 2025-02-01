d4dcdd19201539fddc7395702f66d1662f7693ff
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-01-31 23:57:41 -0300

    Aula 101 - Criação do ícone do elemento na lista

--- 14-projeto-2/scripts.js
+++ 14-projeto-2/scripts.js
@@ -4,6 +4,9 @@ const amount = document.querySelector('#amount');
 const expense = document.querySelector('#expense');
 const category = document.querySelector('#category');
 
+//Elementos da lista
+const expenseList = document.querySelector('ul');
+
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "");
@@ -29,12 +32,21 @@ form.onsubmit = e => {
         amount: amount.value,
         createdAt: new Date(),
     };
+
+    addExpense(newExpense);
 };
 
 function addExpense(newExpense) {
     try {
         const expenseItem = document.createElement('li');
         expenseItem.classList.add('expense');
+
+        const expenseIcon = document.createElement('img');
+        expenseIcon.setAttribute('src', `img/${newExpense.categoryId}.svg`);
+        expenseIcon.setAttribute('alt', newExpense.categoryName);
+
+        expenseItem.append(expenseIcon);
+        expenseList.append(expenseItem);
 
     } catch (error) {
         alert('Não foi possível atualizar a lista de despesas.');
